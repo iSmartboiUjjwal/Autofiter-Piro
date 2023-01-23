cfa1d165f26d25b610f59e68e241eb787bfdc14b
Tushar Patil <86295965+PrinceStarLord@users.noreply.github.com>
2023-01-23 07:40:41 -0600

    Update info.py

--- info.py
+++ info.py
@@ -40,9 +40,9 @@ AUTH_CHANNEL = -1001324138225
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://imdb4gb1:imdb4gb1@cluster0.ovlqoai.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://seriesibot:seriesibot@cluster0.1dtisux.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Series_files')
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'SeriesDB')
 
 #maximum search result buttos count in number#
 MAX_RIST_BTNS = int(environ.get('MAX_RIST_BTNS', "10"))
